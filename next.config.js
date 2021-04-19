27e657e56f8a0183e7ec13c26ab6aa92d742a64c
ondrejch <Simply007@users.noreply.github.com>
2021-04-19 20:12:52 +0200

    remove webpack plugin completely

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const handler = (percentage, message, ...args) => {
-  // e.g. Output each progress message directly to the console:
-  console.info(percentage, message, ...args);
-};
-
 module.exports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove the workaround the issue is fixed
@@ -11,10 +6,4 @@ module.exports = {
     path: "",
   },
   target: "serverless",
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    
-    // config.plugins.push(new webpack.ProgressPlugin(handler));
-
-    return config;
-  }
 };
